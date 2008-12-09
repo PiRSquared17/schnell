663c773826298cb7e8306ae0a21efb228b344d63
azizatif <azizatif@localhost>
2008-12-09 16:41:14 +0000

    Optimized build batch script to be shorter.

--- build.cmd
+++ build.cmd
@@ -1,9 +1,6 @@
 @echo off
 pushd "%~dp0src"
-for %%i in (*.sln) do call :build %%i
+for %%i in (*.sln) do (
+  for %%j in (Debug Release) do %SystemRoot%\Microsoft.NET\Framework\v3.5\msbuild %%i /p:Configuration=%%j
+)
 popd
-goto :EOF
-
-:build
-for %%i in (Debug Release) do %SystemRoot%\Microsoft.NET\Framework\v3.5\msbuild %1 /p:Configuration=%%i
-goto :EOF
