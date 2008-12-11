d8bacecafc806be269c8a9ae82e3992da159a801
dominik hug <dominik.hug@skybow.com>
2008-12-11 19:01:08 +0000

    Changed FindForm.StartPosition to CenterParent.

--- samples/WikiPad/ReplaceForm.Designer.cs
+++ samples/WikiPad/ReplaceForm.Designer.cs
@@ -135,6 +135,7 @@
             this.MinimizeBox = false;
             this.Name = "ReplaceForm";
             this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Replace";
             this.ResumeLayout(false);
             this.PerformLayout();
