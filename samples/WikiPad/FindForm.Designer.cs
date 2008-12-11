dca9d39852c3ef6be197bb255fcbd174d00dd83d
azizatif <azizatif@localhost>
2008-12-11 16:30:34 +0000

    Removed icon on FindForm so that the border appears correctly.

--- samples/WikiPad/FindForm.Designer.cs
+++ samples/WikiPad/FindForm.Designer.cs
@@ -39,26 +39,29 @@ namespace WikiPad {
             // _findLabel
             // 
             this._findLabel.AutoSize = true;
-            this._findLabel.Location = new System.Drawing.Point(13, 13);
+            this._findLabel.Location = new System.Drawing.Point(17, 16);
+            this._findLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this._findLabel.Name = "_findLabel";
-            this._findLabel.Size = new System.Drawing.Size(56, 13);
+            this._findLabel.Size = new System.Drawing.Size(72, 17);
             this._findLabel.TabIndex = 0;
             this._findLabel.Text = "Fi&nd what:";
             // 
             // _searchBox
             // 
-            this._searchBox.Location = new System.Drawing.Point(75, 10);
+            this._searchBox.Location = new System.Drawing.Point(100, 12);
+            this._searchBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this._searchBox.Name = "_searchBox";
-            this._searchBox.Size = new System.Drawing.Size(212, 20);
+            this._searchBox.Size = new System.Drawing.Size(281, 22);
             this._searchBox.TabIndex = 1;
             this._searchBox.TextChanged += new System.EventHandler(this.SearchBox_TextChanged);
             // 
             // _findButton
             // 
             this._findButton.Enabled = false;
-            this._findButton.Location = new System.Drawing.Point(293, 8);
+            this._findButton.Location = new System.Drawing.Point(391, 10);
+            this._findButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this._findButton.Name = "_findButton";
-            this._findButton.Size = new System.Drawing.Size(102, 23);
+            this._findButton.Size = new System.Drawing.Size(136, 28);
             this._findButton.TabIndex = 2;
             this._findButton.Text = "&Find";
             this._findButton.UseVisualStyleBackColor = true;
@@ -67,9 +70,10 @@ namespace WikiPad {
             // _matchCaseCheckBox
             // 
             this._matchCaseCheckBox.AutoSize = true;
-            this._matchCaseCheckBox.Location = new System.Drawing.Point(16, 52);
+            this._matchCaseCheckBox.Location = new System.Drawing.Point(21, 64);
+            this._matchCaseCheckBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this._matchCaseCheckBox.Name = "_matchCaseCheckBox";
-            this._matchCaseCheckBox.Size = new System.Drawing.Size(82, 17);
+            this._matchCaseCheckBox.Size = new System.Drawing.Size(102, 21);
             this._matchCaseCheckBox.TabIndex = 3;
             this._matchCaseCheckBox.Text = "Match &case";
             this._matchCaseCheckBox.UseVisualStyleBackColor = true;
@@ -78,9 +82,11 @@ namespace WikiPad {
             // 
             this._groupBox.Controls.Add(this._downRadioButton);
             this._groupBox.Controls.Add(this._upRadioButton);
-            this._groupBox.Location = new System.Drawing.Point(164, 52);
+            this._groupBox.Location = new System.Drawing.Point(219, 64);
+            this._groupBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this._groupBox.Name = "_groupBox";
-            this._groupBox.Size = new System.Drawing.Size(123, 48);
+            this._groupBox.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this._groupBox.Size = new System.Drawing.Size(164, 59);
             this._groupBox.TabIndex = 4;
             this._groupBox.TabStop = false;
             this._groupBox.Text = "Direction";
@@ -89,9 +95,10 @@ namespace WikiPad {
             // 
             this._downRadioButton.AutoSize = true;
             this._downRadioButton.Checked = true;
-            this._downRadioButton.Location = new System.Drawing.Point(53, 20);
+            this._downRadioButton.Location = new System.Drawing.Point(71, 25);
+            this._downRadioButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this._downRadioButton.Name = "_downRadioButton";
-            this._downRadioButton.Size = new System.Drawing.Size(53, 17);
+            this._downRadioButton.Size = new System.Drawing.Size(64, 21);
             this._downRadioButton.TabIndex = 1;
             this._downRadioButton.TabStop = true;
             this._downRadioButton.Text = "&Down";
@@ -100,9 +107,10 @@ namespace WikiPad {
             // _upRadioButton
             // 
             this._upRadioButton.AutoSize = true;
-            this._upRadioButton.Location = new System.Drawing.Point(7, 20);
+            this._upRadioButton.Location = new System.Drawing.Point(9, 25);
+            this._upRadioButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this._upRadioButton.Name = "_upRadioButton";
-            this._upRadioButton.Size = new System.Drawing.Size(39, 17);
+            this._upRadioButton.Size = new System.Drawing.Size(47, 21);
             this._upRadioButton.TabIndex = 0;
             this._upRadioButton.Text = "&Up";
             this._upRadioButton.UseVisualStyleBackColor = true;
@@ -110,9 +118,10 @@ namespace WikiPad {
             // _cancelButton
             // 
             this._cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this._cancelButton.Location = new System.Drawing.Point(294, 38);
+            this._cancelButton.Location = new System.Drawing.Point(392, 47);
+            this._cancelButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this._cancelButton.Name = "_cancelButton";
-            this._cancelButton.Size = new System.Drawing.Size(101, 23);
+            this._cancelButton.Size = new System.Drawing.Size(135, 28);
             this._cancelButton.TabIndex = 5;
             this._cancelButton.Text = "Cancel";
             this._cancelButton.UseVisualStyleBackColor = true;
@@ -121,10 +130,10 @@ namespace WikiPad {
             // FindForm
             // 
             this.AcceptButton = this._findButton;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this._cancelButton;
-            this.ClientSize = new System.Drawing.Size(417, 113);
+            this.ClientSize = new System.Drawing.Size(556, 139);
             this.Controls.Add(this._cancelButton);
             this.Controls.Add(this._groupBox);
             this.Controls.Add(this._matchCaseCheckBox);
@@ -132,9 +141,11 @@ namespace WikiPad {
             this.Controls.Add(this._searchBox);
             this.Controls.Add(this._findLabel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "FindForm";
+            this.ShowIcon = false;
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Find";
